ed58228d57be37d4b583b0c9524668ae30ec41ff
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-03-09 09:58:50 -0600

    letters and numbers

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+/* Creating an array of characters that will be used to generate the password. */
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+const specialCharacters = [ "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "`", "~", "/", "?", ".", ">", ",", "<", ";", ":", "'", "[", "{", "]", "}"];
+
+function generatePassword() {
+  // console.log("TEST")
+  
+  // Prompt the user for the password criteria
+  
+  generateBtn =alert("Would you like to generate a password?");
+  
+  // password length between 8-128
+  passwordLength = prompt("How long would you like your password to be between 8-128 characters?");
+  console.log(typeof passwordLength, passwordLength)
+  
+  if (!passwordLength) {
+    alert("Please enter a valid number.")
+    return generatePassword ()
+  }
+  
+  if(passwordLength < 8 || passwordLength > 128){
+    window.alert("Please pick between 8-128"); 
+    passwordLength = parseInt(passwordLength)
+    return;
+  }
+  
+  
+  // Lowercase, uppercase, numbers, special characters
+  if (confirm("Would you like your password to contain Uppercase letters?")){
+    choice = passwordLength.concat(upperCase);
+  } if (confirm("Would you like your password to contain Lowercase letters?")){
+    choice = passwordLength.concat(lowerCase);
+  } if (confirm("Would you like your password to contain numbers?")){
+    choice = passwordLength.concat(numbers);
+  } if (confirm("Would you like your password to contain Special Characters?")){
+    choice = passwordLength.concat(specialCharacters);
+  }
+  // Validate the input
+  var randomPassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * choice.length);
+    randomPassword = randomPassword + choice[randomCharacter];
+  }
+  // generate password
+  
+  // Display generated password
+  return randomPassword
+}
+var choice = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,12 +67,4 @@ generateBtn.addEventListener("click", writePassword);
 
 //Homework 3 Hints:
 //use Math.random() and Math.floor() to select a random index from an array.
-//Use promt(), confirm(), and alert() to retrieve input from users.
-
-function generatePassword(length) {
-  let password = ('');
-  const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?><:|}{+_~`-=[]\;',./"
-  let passwordLength = length; 
-
-
-}  
+//Use prompt(), confirm(), and alert() to retrieve input from users.
